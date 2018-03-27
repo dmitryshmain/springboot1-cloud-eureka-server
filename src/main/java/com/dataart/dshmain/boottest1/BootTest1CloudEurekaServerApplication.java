8bf93a9f8772bf13849785c578db36099a6a845c
Dmitry Shmain <dmitry.shmain@gmail.com>
2018-03-27 15:46:08 +0300

    init

---
+++ src/main/java/com/dataart/dshmain/boottest1/BootTest1CloudEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.dataart.dshmain.boottest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootTest1CloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootTest1CloudEurekaServerApplication.class, args);
+	}
+}
