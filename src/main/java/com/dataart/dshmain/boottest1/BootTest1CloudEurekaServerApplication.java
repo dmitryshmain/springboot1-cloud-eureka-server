1fa9b53f9421179d3d85decb5eb848f73bbcf46f
Dmitry Shmain <dmitry.shmain@gmail.com>
2018-03-27 17:51:22 +0300

    updates

--- src/main/java/com/dataart/dshmain/boottest1/BootTest1CloudEurekaServerApplication.java
+++ src/main/java/com/dataart/dshmain/boottest1/BootTest1CloudEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.dataart.dshmain.boottest1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class BootTest1CloudEurekaServerApplication {
 
 	public static void main(String[] args) {
